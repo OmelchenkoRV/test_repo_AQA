e62da1513f5b837b6dadf0d03ceeea7dcba70f80
Rostyslav Omelchenko <omelchenkorv@gmail.com>
2021-04-19 23:34:41 +0300

    Third task added

---
+++ task_3.py
@@ -0,0 +1,9 @@
+
+document_text = open('simple_text.txt', 'r')
+text_string = (document_text.read()).split()
+
+result = {i: text_string.count(i) for i in text_string}
+
+for k, v in result.items():
+    print(k, v)
+document_text.close()
