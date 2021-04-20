aca961c2dc9bbdbd8be9104c1ecda90b1f9d1ed9
Rostyslav Omelchenko <omelchenkorv@gmail.com>
2021-04-20 21:20:57 +0300

    Rewrite according to the task

--- task_3.py
+++ task_3.py
@@ -3,7 +3,7 @@ document_text = open('simple_text.txt', 'r')
 text_string = (document_text.read()).split()
 text_string.sort()
 result = {i: text_string.count(i) for i in text_string}
-
-for k, v in result.items():
-    print(k, v)
 document_text.close()
+with open('output.txt', 'w') as fl:
+    for k, v in result.items():
+        fl.write('{}:{}\n'.format(k, v))
