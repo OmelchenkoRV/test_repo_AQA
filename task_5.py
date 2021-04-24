a5928727605a32efb8b2ccea289d03024e42efec
Rostyslav Omelchenko <omelchenkorv@gmail.com>
2021-04-24 18:27:10 +0300

    Final version

--- task_5.py
+++ task_5.py
@@ -1,24 +1,21 @@
 import csv
-import collections
-import itertools
-count = collections.Counter()
+
 with open('convertcsv.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
+    reader = csv.reader(csvfile)
     max_value = 0
-    max_salaries = []
+    max_salary = []
+    max_salaries = dict()
     for row in reader:
-        count[row['department']] += 1
 
-        if float(row['dollar']) > max_value:
-            max_value = float(row['dollar'])
+        if float(row[-1]) > max_value:
+            max_value = float(row[-1])
 
-        if row['department'] not in max_salaries:
-            max_salaries.append(row)
+        if row[3] not in max_salary:
+            max_salary.append(row[3])
+            max_salaries[row[3]] = row[1], row[2], row[-1]
+        if row[3] in max_salaries and float(row[-1]) > float(max_salaries[row[3]][-1]):
+            max_salaries[row[3]] = row[1], row[2], row[-1]
 
-    print('There are', len(count), 'departments')
-    print('Max salary is ', max_value, '$')
-    print(type(row))
-    newlist = sorted(max_salaries, key=lambda k: k['department'], reverse=True)
-    result = []
-
-print(newlist)
+print(len(max_salaries))
+print(max_value)
+print(max_salaries)
